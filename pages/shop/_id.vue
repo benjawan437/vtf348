847f7d655cf3928e8a83962112512bab73ba6920
benjawn497 <benjawn497@gmail.com>
2020-10-02 22:22:50 +0700

    u

---
+++ pages/shop/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>รายละเอียดสินค้า</h1>
+    <h2>สินค้าหมายเลข {{ this.$route.parms.id}}</h2>
+    <h3>{{ id }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            id: this.$route.parms.id,
+        }
+    },
+}
+</script>
